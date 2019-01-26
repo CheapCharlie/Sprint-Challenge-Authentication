48b79f109a2d41c18d1a40bb319880359c9815f4
Chris Juelfs <CRJuelfs24@comcast.net>
2019-01-26 12:46:43 -0600

    attendance check

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
+//attendance check
 require('dotenv').config(); // load .env variables
 
 const { server } = require('./api/server.js');
-
 const port = process.env.PORT || 3300;
+
+
 server.listen(port, () => {
   console.log(`\n=== Server listening on port ${port}\n`);
 });
